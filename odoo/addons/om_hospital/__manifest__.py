62a5fe02416cb83ad984d0fd03e2a763cc1f5daa
Janusz K. Dlugosz <46513393+delaruuk@users.noreply.github.com>
2024-02-12 07:16:03 -0600

    Currently learning to build a module on platform.

---
+++ odoo/addons/om_hospital/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Hospital Management System',
+    'author': 'Janusz',
+    'website': 'www.usprocom.com',
+    'summary': 'This is me testing out how to make a module for hospital management.',
+    
+    }
