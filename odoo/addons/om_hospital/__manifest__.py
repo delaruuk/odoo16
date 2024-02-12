3f3003223b7fc67905ed0df7373ed4b891b87753
Janusz K. Dlugosz <46513393+delaruuk@users.noreply.github.com>
2024-02-12 14:39:57 -0600

    XML File addition

--- odoo/addons/om_hospital/__manifest__.py
+++ odoo/addons/om_hospital/__manifest__.py
@@ -3,5 +3,8 @@
     'author': 'Janusz',
     'website': 'www.usprocom.com',
     'summary': 'This is me testing out how to make a module for hospital management.',
-    
-    }
+    'data': [
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}
--- odoo/addons/om_hospital/models/patient.py
+++ odoo/addons/om_hospital/models/patient.py
@@ -9,4 +9,4 @@ class HospitalPatient(models.Model):
     age = fields.Integer(string='Age', required=True)
     is_child = fields.Boolean(string='Is Child', required=True)
     notes = fields.Text(string='Notes', required=True)
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', required=True,
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', required=True)
