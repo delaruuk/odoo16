3059ed895b4f870fc01641576be1110c91af6736
Janusz K. Dlugosz <46513393+delaruuk@users.noreply.github.com>
2024-02-15 07:40:55 -0600

    Pushin some stuff from yesterday

---
+++ odoo/addons/procom_testing/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Procom Test Module',
+    'author': 'Janusz',
+    'website': 'www.usprocom.com',
+    'summary': 'This is me testing out a module for Procom.',
+    'data': [
+
+    ]
+}
